9a7e6ed651ecd5a91f5f14565b375808694f88af
Jonne Haß <me@jhass.eu>
2017-04-25 20:22:56 +0200

    fix validator script

--- validator/validate.rb
+++ validator/validate.rb
@@ -3,8 +3,11 @@ require "bundler/setup"
 require "json"
 require "json-schema"
 
-schema = File.read File.expand_path "#{__dir__}/../schemas/1.0/schema.json"
 json = JSON.parse(ARGF.read)
+version = json["version"] || "2.0"
+schema_path = File.expand_path "#{__dir__}/../schemas/#{version}/schema.json"
+abort "Invalid schema version #{version}" unless File.exist? schema_path
+schema = File.read schema_path
 
 messages = JSON::Validator.fully_validate(schema, json)
 
